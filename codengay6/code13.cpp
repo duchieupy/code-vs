334d5b8b225164e7c009edf4f5b4e873f8164e8e
Dang Duc Hieu <hieu123@gmail.com>
2026-05-03 16:00:45 +0700

    codesinh2

---
+++ codengay6/code13.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<math.h>
+#include<chrono>
+#include<string>
+#include<algorithm>
+using namespace std;
+using namespace std::chrono;
+int a[1000];
+string s;
+bool final = false;
+void sinh(string &s){
+    int i = s.size()-2;
+    while(i>=0 && s[i]>=s[i+1]){
+        --i;
+    }
+    if(i==-1){
+        final=true;
+    }
+    else{
+        int j=s.size()-1;
+        while( s[i]>=s[j]) --j;
+        swap(s[i],s[j]);
+        int l=i+1;
+        int r=s.size()-1;
+        while(l<r){
+            swap(s[l],s[r]);
+            --r;
+            ++l;
+        
+        }
+        cout << s << endl;
+    }
+
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int tc=t; 
+        cin >> s;
+        cout << tc << endl;
+        sinh(s);
+    }
+}
